9b56a788945a01186d7e8c998641618fcf1a8ad0
novis10813 <zhizhongz895253@gmail.com>
2022-01-11 21:58:19 +0800

    finish main

--- main.py
+++ main.py
@@ -3,3 +3,28 @@ import pandas as pd
 from agents.DQN import DQN, DuelDQN, DoubleDQN, CERDQN
 from Env import CustomEnv
 
+'''
+df = pd.read_csv()
+'''
+
+env = CustomEnv()
+
+### DQN Agent ###
+agent = DQN(env)
+agent.train()
+#################
+
+### Duel DQN ###
+agent = DuelDQN(env)
+agent.train()
+################
+
+### Double DQN ###
+agent = DoubleDQN(env)
+agent.train()
+################
+
+### CER DQN ###
+agent = CERDQN(env)
+agent.train()
+################
